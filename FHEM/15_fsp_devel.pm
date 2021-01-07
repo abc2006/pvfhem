eed8e04ac261503f11879aa1a315da5c5fefe380
stephan <abc2006@gmx.de>
2021-01-07 11:50:27 +0100

    checksum test added

--- FHEM/15_fsp_devel.pm
+++ FHEM/15_fsp_devel.pm
@@ -8,12 +8,13 @@ use warnings;
 use Digest::CRC qw(crc);
 
 my %requests_02 = (
-	'P003GS' => "5e5030303347530d", # Query General Status ##2 sek
-	'P003PS' => "5e5030303350530d" # Query Power Status ## 2 sek
+	'P003PS' => "5e5030303350530d", # Query Power Status ## 2 sek
+	'P007EMINFO' => "5e50303037454d494e464f0d"
 );
 
 my %requests_30 = (
 	'P003WS' => "5e5030303357530d", # Query Warning Status ##30 sek
+	'P003GS' => "5e5030303347530d", # Query General Status ##2 sek
 	'P004MOD' => "5e503030344d4f440d" # Query Working Mode ## 30 sek
 );
 
@@ -138,7 +139,9 @@ sub fsp_devel_Set($@){
 	my $minInterval = 30;
 	Log3($name,5, "fsp_devel argument $a[1] _Line: " . __LINE__);
   	if ($a[1] eq "?"){
-	Log3($name,5, "fsp_devel argument fragezeichen _Line:" . __LINE__);
+	readingsSingleUpdate($hash,"1_1VERSION","1.06",1);
+	$hash->{VERSION} = 1.13;
+	Log3($name,5, "fsp_devel argument set fragezeichen _Line:" . __LINE__);
 	return $usage;
 	}
   	if ($a[1] eq "cmd"){
@@ -339,10 +342,10 @@ sub fsp_devel_Set($@){
 sub fsp_devel_Get($@){
 	my ($hash, @a) = @_;
 	my $name = $hash->{NAME};
-	my $usage = "Unknown argument $a[1], choose one of P003PI:noArg P003ID:noArg P004VFW:noArg P005VFW2:noArg P003MD:noArg P005PIRI:noArg P005FLAG:noArg P002T:noArg P003ET:noArg P004GOV:noArg P004GOF:noArg P005OPMP:noArg P005GPMP:noArg P006MPPTV:noArg P004LST:noArg P003SV:noArg P003DI:noArg P005BATS:noArg P003DM:noArg P004MAR:noArg P004CFS:noArg P005HECS:noArg P006GLTHV:noArg P004FET:noArg P003FT:noArg P005ACCT:noArg P005ACLT:noArg P006FPADJ:noArg P006FPPF:noArg P005AAPF:noArg";
+	my $usage = "Unknown argument $a[1], choose one of P003PI:noArg P003ID:noArg P004VFW:noArg P005VFW2:noArg P003MD:noArg P005PIRI:noArg P005FLAG:noArg P002T:noArg P003ET:noArg P004GOV:noArg P004GOF:noArg P005OPMP:noArg P005GPMP:noArg P006MPPTV:noArg P004LST:noArg P003SV:noArg P003DI:noArg P005BATS:noArg P003DM:noArg P004MAR:noArg P004CFS:noArg P005HECS:noArg P006GLTHV:noArg P004FET:noArg P003FT:noArg P005ACCT:noArg P005ACLT:noArg P006FPADJ:noArg P006FPPF:noArg P005AAPF:noArg P007EMINFO:noArg";
 	Log3($name,5, "fsp_devel argument $a[1]_Line: " . __LINE__);
   	if ($a[1] eq "?"){
-	Log3($name,5, "fsp_devel argument fragezeichen _Line: " . __LINE__);
+	Log3($name,5, "fsp_devel argument get fragezeichen _Line: " . __LINE__);
 	return $usage;
 	}
 
@@ -460,19 +463,27 @@ sub fsp_devel_Get($@){
 		push(@{$hash->{helper}{addCMD}}, "5e50303036465041444a0d");
 		## kandidat für automatik? 
 	}
+	if ($getcmd eq "QDVPR"){ #Query feeding grid power calibration
+		push(@{$hash->{helper}{addCMD}}, "5e51445650520d");
+	}
 	if ($getcmd eq "P006FPPF"){ #Query feed in Power factor
 		push(@{$hash->{helper}{addCMD}}, "5e50303036465050460d");
+	}
+	if ($getcmd eq "P007EMINFO"){ #Query feed in Power factor
+		push(@{$hash->{helper}{addCMD}}, "5e50303037454d494e464f0d");
+		Log3($name,3, "fsp_devel($name) adding $getcmd to cmdqueue _Line:" . __LINE__);
+		Log3($name,4, "fsp_devel($name) cmdqueue scalar(@{$hash->{helper}{addCMD}}  _Line:" . __LINE__);
 	}
 	if ($getcmd eq "P005AAPF"){ #query auto-adjust PF with power Information (rot gefärbt)
 		push(@{$hash->{helper}{addCMD}}, "5e50303035414150460d");
 		## kandidat für automatik? 
 	}
 	if($hash->{MODE} eq "automatic"){
-		Log3($name,5, "fsp_devel getcmd in automatic mode" . __LINE__);
+		Log3($name,3, "fsp_devel($name) getcmd in automatic mode" . __LINE__);
 		RemoveInternalTimer("send:$name"); # Stoppe Timer
 		fsp_devel_sendRequests("send:$name");
 	} else{
-		Log3($name,5, "fsp_devel getcmd in manual mode" . __LINE__);
+		Log3($name,3, "fsp_devel($name) getcmd in manual mode" . __LINE__);
 		fsp_devel_sendRequests("send:$name");
 	}
 }
@@ -491,13 +502,14 @@ sub fsp_devel_prepareRequests{
 	#fülle das array mit den abzufragenden werten
 	foreach my $key (keys %requests_02){ 
 		push(@{$hash->{actionQueue}}, $requests_02{$key} );
-		Log3($name,4, "fsp_devel prepareRequests key $key value $requests_02{$key}_Line:" . __LINE__);
+		Log3($name,4, "fsp_devel($name) prepareRequests_02 key $key value $requests_02{$key}_Line:" . __LINE__);
 	}
 	my $now = gettimeofday();
+	$hash->{helper}{timer1} = $hash->{helper}{timer1} // 0; ## if helper_timer is not defined, use 0 
 	if($now - $hash->{helper}{timer1} > 30){
 		foreach my $key (keys %requests_30){ 
 			push(@{$hash->{actionQueue}}, $requests_30{$key} );
-			Log3($name,4, "fsp_devel prepareRequests key $key value $requests_30{$key}_Line:" . __LINE__);
+			Log3($name,4, "fsp_devel($name) prepareRequests_30 key $key value $requests_30{$key}_Line:" . __LINE__);
 		}
 	$hash->{helper}{timer1} = gettimeofday();
 	}
@@ -553,7 +565,7 @@ sub fsp_devel_Read($$)
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
 	$hash->{CONNECTION} = "established";
-	Log3($name,4, "fsp_devel jetzt wird gelesen _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel jetzt wird gelesen _Line:" . __LINE__);
         my $buf =  DevIo_SimpleRead($hash);
 	Log3($name,5, "fsp_devel buffer: $buf _Line: " . __LINE__);
 	if (!defined($buf)  || $buf eq "")
@@ -564,6 +576,7 @@ sub fsp_devel_Read($$)
 		return "error" ;
 	}
 	$hash->{helper}{recv} .= $buf; 
+	Log3($name,5, "fsp_devel($name) helper_recv: $hash->{helper}{recv}  _Line: " . __LINE__ );
 
 #	readingsSingleUpdate($hash,"1_recv",$hash->{helper}{recv},1);
 	## check if received Data is complete
@@ -571,12 +584,13 @@ sub fsp_devel_Read($$)
 	my $value = unpack "H*", $hash->{helper}{recv};
 	Log3($name,5, "fsp_devel devpack_hex: $value _Line: " . __LINE__ );
 	$hash->{helper}{received_hex} = $value;
-	if ($value =~ /^5e(.*)....0d$/){
+##	if ($value =~ /.*?5e(.*?)(....)0d/){
+	if ($value =~ /.*?(5e.*?0d)/){
+##	readingsSingleUpdate($hash,"1_checksum",$2,1);
+		##
+##	Log3($name,5, "fsp_devel checksum: $1 _Line: " . __LINE__ );
 	my $dev = pack ('H*',$1);
-#	Log3($name,5, "fsp_devel devpack: $dev _Line: " . __LINE__ );
-#	Log3($name,5, "fsp_devel devpack_helper: $hash->{helper}{recv} _Line: " . __LINE__ );
-	
-#	readingsSingleUpdate($hash,"1_recv_hex",$1,1);
+	Log3($name,5, "fsp_devel($name) rcvMessage: $1 _Line: " . __LINE__ );
 	
 	my @h1 = ($1 =~ /(..)/g);
 	my @val2 = map { pack ("H2", $_) } @h1;	
@@ -585,48 +599,96 @@ sub fsp_devel_Read($$)
 		$out .= $part;
 	}
 	$hash->{helper}{received_ascii} = $out;
-	Log3($name,5, "fsp_devel received_ascii: $out _Line: " . __LINE__ );
+	Log3($name,4, "fsp_devel received_ascii: $out _Line: " . __LINE__ );
 
 #	readingsSingleUpdate($hash,"1_recv_ascii",$out,1);
 	fsp_devel_analyzeAnswer($hash);
 	$hash->{helper}{recv} = "";
 	## Watchdog-Timer entfernen, da Antwort empfangen und verarbeitet.
 	RemoveInternalTimer("watchdog:$name");
+	$value="";
 	fsp_devel_sendRequests("send:$name");
 	}
 	return;
+}
+##########################################
+sub fsp_devel_check_sum{
+## fsp_devel_check_sum($response);
+  my $hash = shift @_;
+  my $name = $hash->{NAME};
+my $debu3 = length($hash->{helper}{received_ascii});
+	Log3($name,5, "fsp_devel check_sum length(received_ascii): $debu3 _Line:" . __LINE__);
+  if(length($hash->{helper}{received_ascii})== 0){
+	Log3($name,5, "fsp_devel fehler:received ist empty or undefined: $hash->{helper}{received_ascii} _Line:" . __LINE__);
+	return (0);	  
+	}
+  my $resp1 = substr ($hash->{helper}{received_ascii} , 0,-3  );
+  my $crc = substr ($hash->{helper}{received_ascii} , -3,2  );
+	Log3($name,5, "fsp_devel check_sum response: $hash->{helper}{received_ascii} _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel check_sum resp1: $resp1 _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel check_sum crc: $crc _Line:" . __LINE__);
+  # ^D0251496161801100008000000
+  # \^(D)(\d{3})(.*)(\d{2})$
+  ( my ($label, $len, $payload) = 
+	  ($resp1 =~ /\^(\w)(\d{3})(.*)$/) )  
+	  or return (0) ;
+	Log3($name,5, "fsp_devel check_sum label: $label _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel check_sum len: $len _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel check_sum payload: $payload _Line:" . __LINE__);
+
+  # compare real length with announced lengthi
+  my $debu = length($hash->{helper}{received_ascii})-5-$len;
+  return (0) if ( length($hash->{helper}{received_ascii})-5-$len ) ;
+	Log3($name,5, "fsp_devel check_sum length(response)-5-len (sollte 0 sein): $debu _Line:" . __LINE__);
+ 
+  my $digest = crc($resp1, 16, 0x0000, 0x0000, 0 , 0x1021, 0, 1);
+ my $debu2 = unpack ('n', $crc ) ; 
+ 	my $crc1 = $digest >> 8; 
+	my $crc2 = $digest & "0b11111111";
+ if ($crc1 eq 0x28 || $crc1 eq 0x0d || $crc1 eq 0x0a){
+	Log3($name,0, "fsp_devel check_sum CRC mismatch before $crc1 _Line:" . __LINE__);
+	$crc1=$crc1+0x01;
+	$crc1 = $crc1 << 8;
+	Log3($name,0, "fsp_devel check_sum CRC mismatch after $crc1 _Line:" . __LINE__);
+	$digest =  $crc1 + $crc2; 
+	}
+ if ($crc2 eq 0x28 || $crc2 eq 0x0d || $crc2 eq 0x0a){
+	Log3($name,0, "fsp_devel check_sum CRC mismatch before $crc2 _Line:" . __LINE__);
+	$crc2=$crc2+0x01;
+	Log3($name,0, "fsp_devel check_sum CRC mismatch after $crc2 _Line:" . __LINE__);
+	$digest =  $crc1 + $crc2; 
+ }
+ return (0) unless $digest == unpack ('n', $crc  )  ;
+	Log3($name,5, "fsp_devel check_sum check: $digest == $debu2 _Line:" . __LINE__);
+	my $order = $label . $len;
+  return ($order, $payload); 
+	  #  sprintf("%04x", unpack ('n', $crc,  )), 
+	  #  sprintf("%04x", $digest) 
 }
 #######################################
 sub fsp_devel_analyzeAnswer
 {
 	my ($hash) = @_;
 	my $name = $hash->{NAME};
-	Log3($name,4, "fsp_devel analyzeAnswer jetzt wird ausgewertet _Line:" . __LINE__);
-	my $recString = $hash->{helper}{received_ascii};
-	Log3($name,5, "fsp_devel analyzeAnswer ascii $recString _Line:" . __LINE__);
-	Log3($name,5, "fsp_devel analyzeAnswer hex $hash->{helper}{received_hex} _Line:" . __LINE__);
-	$recString =~ /(....)(.*)/;
-	my $order = $1;
-	my $data = $2;
-	Log3($name,4, "fsp_devel analyzeAnswer_order $1 _Line:" . __LINE__);
-	Log3($name,4, "fsp_devel analyzeAnswer_data $2 _Line:" . __LINE__);
-	#readingsSingleUpdate($hash,"1_order",$order,1);
-	#readingsSingleUpdate($hash,"1_data",$data,1);
-
-## sortieren
-	if($order eq "D025"){ ##  Query Series Number, P003ID 
-	$data =~ /(..)(.*)/;
-	Log3($name,4, "fsp_devel D025 $1 _Line:" . __LINE__);
-	Log3($name,4, "fsp_devel D025 $2 _Line:" . __LINE__);
-	my $out = substr($2,0,$1);
-		readingsSingleUpdate($hash,"SerialNumber",$out,1);
-	}elsif($order eq "D101"){ ## Query Power Status, P003PS, 5e5030303350530d
+	Log3($name,3, "fsp_devel analyzeAnswer jetzt wird ausgewertet _Line:" . __LINE__);
+	my ($order,$data) = fsp_devel_check_sum($hash);
+	Log3($name,5, "fsp_devel analyzeAnswer order: $order _Line:" . __LINE__);
+	Log3($name,5, "fsp_devel analyzeAnswer data: $data _Line:" . __LINE__);
+	if (!length($order) || !length($data)){
+		Log3($name,5, "fsp_devel analyzeAnswer Error _Line:" . __LINE__);
+		return (0);	
+	}
 	my @splits = split(",",$data);
-	 readingsBeginUpdate($hash);
+
+	if($order eq "D025"){ ##  Query Series Number, P003ID 
+		readingsSingleUpdate($hash,"SerialNumber",$splits[0],1);
+	}elsif($order eq "D101"){ ## Query Power Status, P003PS, 5e5030303350530d
+	
+       		readingsBeginUpdate($hash);
 	 	readingsBulkUpdate($hash,"Solar_input_power_1",int($splits[0]),1);
 	 	readingsBulkUpdate($hash,"Solar_input_power_2",int($splits[1]),1);
 	 	readingsBulkUpdate($hash,"Solar_input_power_total",int($splits[0]+$splits[1]),1);
-	 	readingsBulkUpdate($hash,"Battery_Power",$splits[2],1);
+##	 	readingsBulkUpdate($hash,"Battery_Power",$splits[2],1); Field is empty in original answer
 	 	readingsBulkUpdate($hash,"AC_input_active_Power_R",int($splits[3]),1);
 	 	readingsBulkUpdate($hash,"AC_input_active_Power_S",int($splits[4]),1);
 	 	readingsBulkUpdate($hash,"AC_input_active_Power_T",int($splits[5]),1);
@@ -640,17 +702,22 @@ sub fsp_devel_analyzeAnswer
 	 	readingsBulkUpdate($hash,"AC_output_appearent_Power_T",int($splits[13]),1);
 	 	readingsBulkUpdate($hash,"AC_output_appearent_Power_total",int($splits[14]),1);
 	 	readingsBulkUpdate($hash,"AC_output_power_percentage",int($splits[15]),1);
+		$splits[16] = $splits[16] == 0 ? "disconnected" : "connected";
 	 	readingsBulkUpdate($hash,"AC_output_connect_status",$splits[16],1);
+		$splits[17] = $splits[17] == 0 ? "idle" : "working";
 	 	readingsBulkUpdate($hash,"Solar_input_work_status_1",$splits[17],1);
+		$splits[18] = $splits[18] == 0 ? "idle" : "working";
 	 	readingsBulkUpdate($hash,"Solar_input_work_status_2",$splits[18],1);
+		$splits[19] = $splits[19] == 0 ? "standby" : $splits[19] == 1 ? "charge" : "discharge";
 	 	readingsBulkUpdate($hash,"Battery_Power_direction",$splits[19],1);
+		$splits[20] = $splits[20] == 0 ? "standby" : $splits[20] == 1 ? "AC->DC" : "DC->AC";
 	 	readingsBulkUpdate($hash,"DC-AC_Power_direction",$splits[20],1);
+		$splits[21] = $splits[21] == 0 ? "standby" : $splits[21] == 1 ? "input" : "output";
 	 	readingsBulkUpdate($hash,"Line_Power_direction",$splits[21],1);
 	 readingsEndUpdate($hash,1);
 	
 	
 	}elsif($order eq "D047"){ ## Query Warning Status, P003WS, 5e5030303357530d
-	my @splits = split(",",$data);
 	 readingsBeginUpdate($hash);
 	 	readingsBulkUpdate($hash,"Solar_input_loss_1",int($splits[0]),1);
 	 	readingsBulkUpdate($hash,"Solar_input_loss_2",int($splits[1]),1);
@@ -677,54 +744,60 @@ sub fsp_devel_analyzeAnswer
 	 readingsEndUpdate($hash,1);
 	
 	}elsif($order eq "D030"){ ## Query feed in grid calibration, P006FPADJ, 5e50303036465041444a0d
+		if(scalar(@splits) == 8)
+		{	
+		 readingsBeginUpdate($hash);
+		my $factor= undef;
+			$factor = $splits[0] == 0 ? -1 : 1;
+			readingsBulkUpdate($hash,"grid_power_calibration_total",int($splits[1])*$factor,1);
+			$factor = $splits[2] == 0 ? -1 : 1;
+			readingsBulkUpdate($hash,"grid_power_calibration_R",int($splits[3])*$factor,1);
+			$factor = $splits[4] == 0 ? -1 : 1;
+			readingsBulkUpdate($hash,"grid_power_calibration_S",int($splits[5])*$factor,1);
+			$factor = $splits[6] == 0 ? -1 : 1;
+			readingsBulkUpdate($hash,"grid_power_calibration_T",int($splits[7])*$factor,1);
 	
-	my @splits = split(",",$data);
-	
-	 readingsBeginUpdate($hash);
-	my $factor= undef;
-		$factor = $splits[0] == 0 ? -1 : 1;
-		readingsBulkUpdate($hash,"grid_power_calibration_total",int($splits[1])*$factor,1);
-		$factor = $splits[2] == 0 ? -1 : 1;
-		readingsBulkUpdate($hash,"grid_power_calibration_R",int($splits[3])*$factor,1);
-		$factor = $splits[4] == 0 ? -1 : 1;
-		readingsBulkUpdate($hash,"grid_power_calibration_S",int($splits[5])*$factor,1);
-		$factor = $splits[6] == 0 ? -1 : 1;
-		readingsBulkUpdate($hash,"grid_power_calibration_T",int($splits[7])*$factor,1);
-
-	 readingsEndUpdate($hash,1);
-	}elsif($order eq "D005"){ ## Query Working Mode, P004MOD, 5e503030344d4f440d
+			 readingsEndUpdate($hash,1);
+ 		} elsif(scalar(@splits) == 6)
+ 		{
+		 readingsBeginUpdate($hash);
+			readingsBulkUpdate($hash,"EMINFO_A",int($splits[0]),1);
+			readingsBulkUpdate($hash,"EMINFO_default_feedin_power",int($splits[1]),1);
+			readingsBulkUpdate($hash,"EMINFO_actual_PV_power",int($splits[2]),1);
+			readingsBulkUpdate($hash,"EMINFO_actual_feedin_power",int($splits[3]),1);
+			readingsBulkUpdate($hash,"EMINFO_actual_reserved_power",int($splits[4]),1);
+			readingsBulkUpdate($hash,"EMINFO_F",int($splits[5]),1);
+		 readingsEndUpdate($hash,1);
+		}
+ 	}elsif($order eq "D005"){ ## Query Working Mode, P004MOD, 5e503030344d4f440d
 	my $state;
-		if($data ==0){
+		if($splits[0] ==0){
 		$state = "Power on Mode";
-		} elsif($data ==1){
+		} elsif($splits[0] ==1){
 		$state = "Standby Mode";
-		} elsif($data ==2){
+		} elsif($splits[0] ==2){
 		$state = "Bypass Mode";
-		} elsif($data ==3){
+		} elsif($splits[0] ==3){
 		$state = "Battery Mode";
-		} elsif($data ==4){
+		} elsif($splits[0] ==4){
 		$state = "Fault Mode";
-		} elsif($data ==5){
+		} elsif($splits[0] ==5){
 		$state = "Hybrid mode";
-		} elsif($data ==6){
+		} elsif($splits[0] ==6){
 		$state = "Charge Mode";
 		}
 
 	readingsSingleUpdate($hash,"working_mode",$state,1);
 	
 	}elsif($order eq "D008"){ ## Query the maximum output Power for feeding Grid P005GPMP, 5e5030303547504d500d
-	readingsSingleUpdate($hash,"max_feeding_power_pw",int($data),1);
+	readingsSingleUpdate($hash,"max_feeding_power_pw",int($splits[0]),1);
 	
-	Log3($name,4, "fsp_devel analyzeAnswer data $data _Line:" . __LINE__);
+	Log3($name,4, "fsp_devel analyzeAnswer splits0: $splits[0] _Line:" . __LINE__);
 	
 	
 	}elsif($order eq "D110"){ ## Query Power Status, P003GS, 5e5030303347530d
-	my @splits = split(",",$data);
-
-	Log3($name,4, "fsp_devel analyzeAnswer data $data _Line:" . __LINE__);
 	my $leng = @splits;
 	Log3($name,4, "fsp_devel analyzeAnswer length $leng _Line:" . __LINE__);
-
 	Log3($name,4, "fsp_devel analyzeAnswer 0 $splits[0] _Line:" . __LINE__);
 
 	 readingsBeginUpdate($hash);
@@ -734,7 +807,15 @@ sub fsp_devel_analyzeAnswer
 	 	readingsBulkUpdate($hash,"Solar_input_current_2",int($splits[3])/100,1);
 	 	readingsBulkUpdate($hash,"Battery_voltage",int($splits[4])/10,1);
 	 	readingsBulkUpdate($hash,"Battery_capacity",int($splits[5]),1);
-	 	readingsBulkUpdate($hash,"Battery_current",$splits[6]/10,1);
+		my $powerdirection = ReadingsVal($name,"Battery_Power_direction","");
+		my $battpower;
+	        my $powerdir = 1;	
+		if(length($powerdirection) && $powerdirection eq "charge"){
+			$powerdir = -1;
+		}
+		$battpower = ($splits[4]/10) * ($splits[6]/10) * $powerdir;
+	 	readingsBulkUpdate($hash,"Battery_current",($splits[6]/10)*$powerdir,1);
+	 	readingsBulkUpdate($hash,"Battery_Power",$battpower,1);
 	 	readingsBulkUpdate($hash,"AC_input_voltage_R",int($splits[7]),1);
 	 	readingsBulkUpdate($hash,"AC_input_voltage_S",int($splits[8]),1);
 	 	readingsBulkUpdate($hash,"AC_input_voltage_T",int($splits[9]),1);
